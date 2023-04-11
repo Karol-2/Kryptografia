e3760de940dfc55d391f0eaeea1cb7ddd316278d
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-04-11 18:03:57 +0200

    Dodanie opcjonalnego czytania klucza

--- Obrazki/block.py
+++ Obrazki/block.py
@@ -2,24 +2,31 @@ from PIL import Image
 import hashlib
 import random
 
-input_image = Image.open("plainfb.bmp")
-
 block_size = 8
-image_data = input_image.tobytes()
-size = input_image.size
 
+# Read input image
+input_image = Image.open("plain.bmp")
+size = input_image.size
+image_data = input_image.tobytes()
+
+# Read key from file
+try:
+    with open("key.txt", "rb") as f:
+        key = f.read()
+    if len(key) != block_size:
+        raise ValueError(f"Key size must be {block_size} bytes")
+except FileNotFoundError:
+    keys = []
+    for x in range(block_size):
+        key = hashlib.md5(str(random.random() * x).encode("UTF-8")).digest()
+        keys.append(key)
 # ECB
 new_data = []
-keys = []
-for x in range(block_size):
-    key = hashlib.md5(str(random.random() * x).encode("UTF-8")).digest()
-    keys.append(key)
-
 for x in range(size[0]):
     for y in range(size[1]):
         pp = x * size[0] + y  # pixel position
         op = image_data[pp]  # original pixel
-        pta = op ^ keys[x % block_size][y % block_size]     # pixel to add
+        pta = op ^ key[y % block_size]  # pixel to add
         new_data.append(pta)
 
 output_image = input_image.copy()
@@ -30,8 +37,8 @@ print("ECB Done")
 # CBC
 new_key = 13
 new_data = [image_data[0] ^ new_key]
-for x in range(size[0]*size[1]):
-    new_data.append(new_data[x-1] ^ image_data[x] ^ keys[x%64//8][x%8])
+for x in range(size[0] * size[1]):
+    new_data.append(new_data[x - 1] ^ image_data[x] ^ key[x % block_size])
 
 output_image = input_image.copy()
 output_image.frombytes(bytes(new_data))
